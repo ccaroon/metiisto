b6abab3a04dfb5791010d1a364be713b98a6aac9
Craig N. Caroon <craig-web@ccaroon.org>
2012-02-10 20:55:35 -0500

    * bin/mdb.pl     - changed hash bang to use /usr/bin/env * bin/metiisto     - changaed hash bang to use /bin/bash * sql/populate_defaults.sql     - new file specifying some app defaults

--- bin/mdb.pl
+++ bin/mdb.pl
@@ -1,4 +1,4 @@
-#!/bin/env perl
+#!/usr/bin/env perl
 use strict;
 
 use lib "$ENV{METIISTO_HOME}/sql/migrations";
---
+++ sql/populate_defaults.sql
@@ -0,0 +1,12 @@
+insert into preferences (user_id,name,value) values (1,'theme','humanity');
+insert into preferences (user_id,name,value) values (1,'jira_host','www.yourjirahost.com');
+insert into preferences (user_id,name,value) values (1,'jira_username','Jira Username Here');
+insert into preferences (user_id,name,value) values (1,'jira_password','Jira Password Here');
+insert into preferences (user_id,name,value) values (1,'jira_filter_id','1');
+insert into preferences (user_id,name,value) values (1,'report_recipients','name@email.com,other_name@email.com');
+insert into preferences (user_id,name,value) values (1,'encryption_passphrase','CURRENTLY NOT USED');
+insert into preferences (user_id,name,value) values (1,'smtp_host','smtp.google.com');
+insert into preferences (user_id,name,value) values (1,'smtp_port','142');
+insert into preferences (user_id,name,value) values (1,'smtp_user','SMTP username');
+insert into preferences (user_id,name,value) values (1,'smtp_pass','SMTP passwd');
+
