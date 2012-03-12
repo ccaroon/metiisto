20a84474a0df1db2c2c3308160f7fa33fdc49812
Craig N. Caroon <craig-web@caroon.org>
2012-03-12 12:18:36 -0400

    * #20 - Controllers should declare_routes() on import     - lib/Metiisto/Controller/Base.pm -- added 'import()' which calls 'declare_routes()'     - lib/metiisto.pm -- removed calls to declare_routes() on controllers

--- lib/metiisto.pm
+++ lib/metiisto.pm
@@ -6,31 +6,14 @@ use Dancer::Plugin::Database;
 our $VERSION = '1.0';
 
 use Metiisto::Controller::Home;
-Metiisto::Controller::Home->declare_routes();
-
 use Metiisto::Controller::Entries;
-Metiisto::Controller::Entries->declare_routes();
-
 use Metiisto::Controller::Users;
-Metiisto::Controller::Users->declare_routes();
-
 use Metiisto::Controller::Workdays;
-Metiisto::Controller::Workdays->declare_routes();
-
 use Metiisto::Controller::Todos;
-Metiisto::Controller::Todos->declare_routes();
-
 use Metiisto::Controller::Lists;
-Metiisto::Controller::Lists->declare_routes();
-
 use Metiisto::Controller::Notes;
-Metiisto::Controller::Notes->declare_routes();
-
 use Metiisto::Controller::Countdowns;
-Metiisto::Controller::Countdowns->declare_routes();
-
 use Metiisto::Controller::Reports;
-Metiisto::Controller::Reports->declare_routes();
 ################################################################################
 hook before => sub
 {
