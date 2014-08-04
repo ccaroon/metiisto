5602a4457d6d32635ed61bfb61ee5cd2ab721ff7
Craig N. Caroon <craig-web@caroon.org>
2014-08-04 16:53:26 -0400

    * Added Fog weather icon

--- lib/Metiisto/Weather.pm
+++ lib/Metiisto/Weather.pm
@@ -13,6 +13,7 @@ use constant WI_ICON_MAP => {
     "Scattered Clouds"             => 'wi-day-sunny-overcast',
     "Fair"                         => 'wi-day-sunny',
     "Rain"                         => 'wi-rain',
+    "Fog"                          => 'wi-fog',
     "Light Rain"                   => 'wi-sprinkle',
     "Overcast"                     => 'wi-cloudy',
     "Thunderstorm"                 => 'wi-thunderstorm',
